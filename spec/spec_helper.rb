867f4a2ffdf5fee55a7503da14fc63fc26e2a308
Karol Galanciak <karol.galanciak@gmail.com>
2016-12-01 12:43:40 +0100

    require coveralls before requiring the rest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "active_model_attributes"
-require "pry"
 require 'coveralls'
 
 Coveralls.wear!
+
+require "active_model_attributes"
+require "pry"
