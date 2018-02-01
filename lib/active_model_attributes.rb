d690c5fd73bb3fec56a7e906cf014e0b4f41d31f
Alan Kennedy <alankennedy100@gmail.com>
2018-02-01 13:55:58 +0000

    Allow untyped attributes (as Rails does)
    Allow untyped attributes by defaulting to the `ActiveModel::Type::Value` type.
    
    Rails 5.2 will support typed attributes on ActiveModel, however their
    implementation supports untyped attributes as well.
    
    This change means that the gem is compatible with the upcoming change in Rails.

--- lib/active_model_attributes.rb
+++ lib/active_model_attributes.rb
@@ -15,7 +15,7 @@ module ActiveModelAttributes
     SERVICE_ATTRIBUTES = %i(default user_provided_default).freeze
     private_constant :NO_DEFAULT_PROVIDED
 
-    def attribute(name, cast_type, **options)
+    def attribute(name, cast_type = ActiveModel::Type::Value.new, **options)
       self.attributes_registry = attributes_registry.merge(name => [cast_type, options])
 
       define_attribute_reader(name, options)
@@ -37,7 +37,10 @@ module ActiveModelAttributes
     def define_attribute_writer(name, cast_type, options)
       wrapper = Module.new do
         define_method "#{name}=" do |val|
-          deserialized_value = ActiveModel::Type.lookup(cast_type, **options.except(*SERVICE_ATTRIBUTES)).cast(val)
+          if cast_type.is_a?(Symbol)
+            cast_type = ActiveModel::Type.lookup(cast_type, **options.except(*SERVICE_ATTRIBUTES))
+          end
+          deserialized_value = cast_type.cast(val)
           instance_variable_set("@#{name}", deserialized_value)
         end
       end
